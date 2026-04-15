30c8f929ebe10f10be15a8068877d4f9a301e2ab
hendycode <ratemoneon@gmail.com>
2026-04-15 10:59:57 +0300

    refresh

--- run_bot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title RedditVideoMakerBot - Generate Video
-cd /d "%~dp0"
-echo Generating Reddit video...
-echo.
-py -3.11 main.py
-pause
--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title RedditVideoMakerBot
-cd /d "%~dp0"
-echo Starting RedditVideoMakerBot GUI...
-echo.
-py -3.11 GUI.py
-pause
